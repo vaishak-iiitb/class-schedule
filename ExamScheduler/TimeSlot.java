d72d6aa3929938766c69655bfa381559780728e6
harshitKrishna-main <Harshit.Krishna@iiitb.ac.in>
2024-11-29 21:02:36 +0530

    Update TimeSlot.java

--- ExamScheduler/TimeSlot.java
+++ ExamScheduler/TimeSlot.java
@@ -1,13 +1,15 @@
 public class TimeSlot {
 
-    private int slotNumber;
-    private String timeRange;
+    private int slotNumber;     // Identifier for the time slot (e.g., 1 or 2)
+    private String timeRange;   // Time range for the slot (e.g., "9:30 am - 12:30 pm")
 
-    public TimeSlot(int slotNumber, String timeRange) {
+    public TimeSlot(int slotNumber, String timeRange) {  //Parameterized constructor to initialize a TimeSlot object with specific details.
         this.slotNumber = slotNumber;
         this.timeRange = timeRange;
     }
 
+
+    //getters
     public int getSlotNumber() {
         return slotNumber;
     }
