8baebaa0596c0d68daa184f287bb987c1a374b09
harshitKrishna-main <Harshit.Krishna@iiitb.ac.in>
2024-11-29 21:02:14 +0530

    Update Room.java

--- ExamScheduler/Room.java
+++ ExamScheduler/Room.java
@@ -1,13 +1,19 @@
 public class Room {
 
-    private String roomName;
-    private int capacity;
+    private String roomName;  // Name of the room
+    private int capacity;     // Capacity of the room
 
     public Room(String roomName) {
+        /*
+        Constructor to initialize the Room object with a given name.
+        Sets a default capacity of 30 for the room.
+        */
+
         this.roomName = roomName;
         this.capacity = 30; // Default capacity, can be modified if needed
     }
 
+    //getters
     public String getRoomName() {
         return roomName;
     }
