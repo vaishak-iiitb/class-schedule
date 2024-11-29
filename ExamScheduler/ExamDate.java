e951cf4cc073241ba61d71d8945e9a5ac22df068
harshitKrishna-main <Harshit.Krishna@iiitb.ac.in>
2024-11-29 21:00:59 +0530

    Update ExamDate.java

--- ExamScheduler/ExamDate.java
+++ ExamScheduler/ExamDate.java
@@ -3,17 +3,20 @@ import java.util.List;
 
 public class ExamDate {
 
-    private String date;
-    private List<TimeSlot> availableSlots;
+    private String date;                        //String of date in DD/MM/YYYY format
+    private List<TimeSlot> availableSlots;      //List of available slots on the given date
 
-    public ExamDate(String date) {
+    public ExamDate(String date) {   
+        /* Constructor which initializes the object with the given date, 
+        and populates the list of available slots with default slots */
         this.date = date;
         this.availableSlots = new ArrayList<>();
-        // Add default time slots
+        // Adds default time slots
         availableSlots.add(new TimeSlot(1, "9:30 am - 12:30 pm"));
         availableSlots.add(new TimeSlot(2, "2:00 pm - 5:00 pm"));
     }
 
+    //getters
     public String getDate() {
         return date;
     }
